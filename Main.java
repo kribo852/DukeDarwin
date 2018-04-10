71b3d91701679978f000397c08fa974986224c4e
Borg <kribo852@student.liu.se>
2018-04-10 02:20:06 +0000

    Modified reflection code to use only constructor of LifeForm, removed example-code in Main-class

--- Main.java
+++ Main.java
@@ -38,9 +38,7 @@ abstract class Habitat{
 	
 	protected Lifeform newInstance(Collection genome, Class lifeformClass){
 		try {
-			Class[] params=new Class[1];
-			params[0]=Collection.class;
-			Constructor ct = lifeformClass.getConstructor(params);
+			Constructor ct = lifeformClass.getConstructors()[0];//lifeform has only one constructor
 			Object[] argslist=new Object[]{genome};
 			return(Lifeform)ct.newInstance(argslist);
 		}catch(Exception e){
@@ -51,49 +49,4 @@ abstract class Habitat{
 		return null;
 	}
 	
-}
-
-class SpeciesContainer extends Habitat{
-	final int saveinterval = 15;
-	final int habitatsize=25;
-	
-	public void run(Class lifeformClass){
-		genomes = new TreeMap<Double, Collection> ();
-		
-		for(int i=0; i<habitatsize; i++){
-			Lifeform placeholder = newInstance(null, lifeformClass);
-			Collection c=placeholder.newGenome();
-			Lifeform lifeform = newInstance(c, lifeformClass);
-			lifeform.run();
-			genomes.put(lifeform.getScore(), c);
-		}
-		
-		long secs = System.currentTimeMillis()/1000;
-		
-		while(true) {
-			
-			for(Entry<Double, Collection> entry : genomes.descendingMap().entrySet()) {
-				if(new Random().nextDouble()<0.1){
-					Lifeform mutator = newInstance(null, lifeformClass);
-					Collection c=mutator.mutate(entry.getValue());
-					Lifeform lifeform = newInstance(c, lifeformClass);
-					lifeform.run();
-					genomes.put(lifeform.getScore(), c);
-					if(genomes.size()>habitatsize){
-						genomes.pollFirstEntry();
-					}
-					break;
-				}
-			}
-			
-			
-			if(System.currentTimeMillis()/1000>secs+saveinterval) {
-				secs = System.currentTimeMillis()/1000;
-				Lifeform lifeform = newInstance(genomes.lastEntry().getValue(), lifeformClass);
-				lifeform.run();
-				lifeform.output();
-				System.out.println("--------------------");
-			}
-		}
-	}
 }
