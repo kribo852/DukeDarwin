b7924e4d5eecd18c8a5025ab6e29b4e507f2f2de
kribo852 <kribo852@student.liu.se>
2019-04-14 22:19:16 +0200

    added a normal distribution function as a test

--- app/examples/SolarpaneldistLifeform.java
+++ app/examples/SolarpaneldistLifeform.java
@@ -36,13 +36,13 @@ public class SolarpaneldistLifeform extends Lifeform<Shape, ArrayList<Shape>> {
 	
 	@Override
 	public ArrayList<Shape> mutate(final ArrayList<Shape> genome1, final ArrayList<Shape> genome2) {
-		ArrayList<Shape> result = new ArrayList();		
-		if(new Random().nextBoolean()) {
-			result.add(genome1.get(0).mutate(readings, peakvalue));
-			result.add(genome2.get(1).mutate(readings, peakvalue));
-		} else {
-			result.add(genome2.get(0).mutate(readings, peakvalue));
-			result.add(genome1.get(1).mutate(readings, peakvalue));
+		ArrayList<Shape> result = new ArrayList<>();	
+		for(int i=0; i<genome1.size(); i++) {
+			if(new Random().nextBoolean()) {
+				result.add(genome1.get(i).mutate(readings, peakvalue));
+			} else {
+				result.add(genome2.get(i).mutate(readings, peakvalue));
+			}
 		}
 		return result;
 	}
@@ -57,20 +57,24 @@ public class SolarpaneldistLifeform extends Lifeform<Shape, ArrayList<Shape>> {
 		if(jframe == null) {
 			jframe = new JFrame();
 			jframe.setVisible(true);
-			jframe.setSize(1000, 1000);		
+			jframe.setSize(800, 1250);		
 			jframe.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		}
 
-		BufferedImage bufferedImage1 = new BufferedImage(1000, 500, 1);
-		BufferedImage bufferedImage2 = new BufferedImage(1000, 500, 1);
+		BufferedImage bufferedImage1 = new BufferedImage(800, 350, 1);
+		BufferedImage bufferedImage2 = new BufferedImage(800, 350, 1);
+		BufferedImage bufferedImage3 = new BufferedImage(800, 350, 1);
 		
 		System.out.println("Score: "+score);
 		genome.get(0).printdata(bufferedImage1, readings, middlepoint);
 		System.out.println();
 		genome.get(1).printdata(bufferedImage2, readings, middlepoint);
+		System.out.println();
+		genome.get(2).printdata(bufferedImage3, readings, middlepoint);
 
 		jframe.getGraphics().drawImage(bufferedImage1, 0, 0, null);
-		jframe.getGraphics().drawImage(bufferedImage2, 0, 500, null);
+		jframe.getGraphics().drawImage(bufferedImage2, 0, 350, null);
+		jframe.getGraphics().drawImage(bufferedImage3, 0, 685, null);
 	}
 
 	@Override	
@@ -78,29 +82,55 @@ public class SolarpaneldistLifeform extends Lifeform<Shape, ArrayList<Shape>> {
 		ArrayList<Shape> rtn = new ArrayList<>();
 		rtn.add(SeconddegreeEquation.newGenome(readings, peakvalue));
 		rtn.add(CustomShape.newGenome(readings, peakvalue));
+		rtn.add(NormalDistribution.newGenome(readings, peakvalue));
 		return rtn;
 	}
 
 	@Override
 	public void run() {
-		score -= genome.get(0).cost(readings, middlepoint);
-		score -= genome.get(1).cost(readings, middlepoint);
+		genome.forEach(gene -> score -= gene.cost(readings, middlepoint));
 	}
 }
 
-interface Shape{
+abstract class Shape{
+	static Random RND = new Random();
 
-	public double cost(final double[] readings, final double middlepoint);
+	public double cost(final double[] readings, final double middlepoint) {
+		double cost = 0;
+		for (int i=0; i<readings.length; i++) {
+			double position = i-middlepoint;
+			cost += Math.pow((readings[i]-getHeightAtPosition(position)), 2);
+		}
+		return Math.sqrt(cost);
+	}
 
-	public double area();	
+	public abstract double area();	
 
-	public void printdata(final BufferedImage bufferedImage, final double[] readings, final double middlepoint);
+	public abstract void printdata(final BufferedImage bufferedImage, final double[] readings, final double middlepoint);
 
-	public Shape mutate(final double[] readings, final double peakvalue);
+	public abstract Shape mutate(final double[] readings, final double peakvalue);
 
+	public void printGraph(final BufferedImage bufferedImage, final double[] readings, final double middlepoint) {
+		Graphics graphics = bufferedImage.getGraphics();
+		graphics.setColor(Color.RED);
+		double scale = 0.075;
+		
+		for(int i=0; i<bufferedImage.getWidth(); i++) {
+			double position = ((double)i/bufferedImage.getWidth())*readings.length;
+			double height = scale*getHeightAtPosition(position-middlepoint);
+			graphics.fillRect(i, bufferedImage.getHeight()-(int)height, 1, 1);
+		}
+		graphics.setColor(Color.blue);
+		for(int i=0; i<readings.length; i++) {
+			graphics.fillRect((i*bufferedImage.getWidth())/readings.length-5, 
+				bufferedImage.getHeight()-5-(int)(scale*readings[i]), 10, 10);
+		}
+	}
+	
+	abstract double getHeightAtPosition(double position);
 }
 
-final class SeconddegreeEquation implements Shape{
+final class SeconddegreeEquation extends Shape{
 	double hightscaler;// 
 	double roots;//one positive and one negative
 
@@ -110,18 +140,7 @@ final class SeconddegreeEquation implements Shape{
 	}
 
 	public static SeconddegreeEquation newGenome(final double[] readings, final double peakvalue){
-		Random RND = new Random();
 		return new SeconddegreeEquation(RND.nextDouble()*peakvalue, RND.nextDouble()*readings.length);
-	}
-
-	@Override
-	public double cost(final double[] readings, final double middlepoint) {
-		double cost = 0;
-		for (int i=0; i<readings.length; i++) {
-			double position = i-middlepoint;
-			cost += Math.pow(readings[i]-getHeightAtPosition(position), 2);
-		}
-		return Math.sqrt(cost);
 	}
 
 	@Override
@@ -136,26 +155,11 @@ final class SeconddegreeEquation implements Shape{
 		System.out.println("Area: " + area());
 		System.out.println("Scaler: " + hightscaler);
 		System.out.println("Roots: +-" + roots/2 +" hours");
-
-		Graphics graphics = bufferedImage.getGraphics();
-		graphics.setColor(Color.RED);
-		double scale = 0.1;
-		
-		for(int i=0; i<bufferedImage.getWidth(); i++) {
-			double position = ((double)i/bufferedImage.getWidth())*readings.length;
-			double height = scale*getHeightAtPosition(position-middlepoint);
-			graphics.fillRect(i, bufferedImage.getHeight()-(int)height, 1, 1);
-		}
-		graphics.setColor(Color.blue);
-		for(int i=0; i<readings.length; i++) {
-			graphics.fillRect((i*bufferedImage.getWidth())/readings.length-5, 
-				bufferedImage.getHeight()-5-(int)(scale*readings[i]), 10, 10);
-		}
+		printGraph(bufferedImage, readings, middlepoint);
 	}
 
 	@Override
 	public SeconddegreeEquation mutate(final double[] readings, final double peakvalue){
-		Random RND = new Random();
 		if(RND.nextInt(10) == 0) {
 			return newGenome(readings, peakvalue);
 		}		
@@ -174,7 +178,7 @@ final class SeconddegreeEquation implements Shape{
 		return child;
 	}
 
-	private double getHeightAtPosition(final double position) {
+	double getHeightAtPosition(final double position) {
 		double expectedheight = -hightscaler*
 			(position - roots)*
 			(position + roots);
@@ -183,7 +187,7 @@ final class SeconddegreeEquation implements Shape{
 	}
 }
 
-final class CustomShape implements Shape{
+final class CustomShape extends Shape{
 	double height;
 	double boxlength;
 	double expcoefficient;
@@ -195,19 +199,8 @@ final class CustomShape implements Shape{
 	}
 
 	public static CustomShape newGenome(final double[] readings, final double peakvalue){
-		Random RND = new Random();
 		return new CustomShape(RND.nextDouble()*peakvalue, 
 			RND.nextDouble()*readings.length, RND.nextDouble());
-	}
-
-	@Override
-	public double cost(final double[] readings, final double middlepoint) {
-		double cost = 0;
-		for (int i=0; i<readings.length; i++) {
-			double position = i-middlepoint;
-			cost += Math.pow((readings[i]-getHeightAtPosition(position)), 2);
-		}
-		return Math.sqrt(cost);
 	}
 
 	@Override
@@ -220,26 +213,11 @@ final class CustomShape implements Shape{
 	public void printdata(final BufferedImage bufferedImage, final double[] readings, final double middlepoint) {
 		System.out.println("Cost for custom shape: "+cost(readings, middlepoint));
 		System.out.println("Area: " + area());
-
-		Graphics graphics = bufferedImage.getGraphics();
-		graphics.setColor(Color.RED);
-		double scale = 0.1;
-		
-		for(int i=0; i<bufferedImage.getWidth(); i++) {
-			double position = ((double)i/bufferedImage.getWidth())*readings.length;
-			double height = scale*getHeightAtPosition(position-middlepoint);
-			graphics.fillRect(i, bufferedImage.getHeight()-(int)height, 1, 1);
-		}
-		graphics.setColor(Color.blue);
-		for(int i=0; i<readings.length; i++) {
-			graphics.fillRect((i*bufferedImage.getWidth())/readings.length-5, 
-				bufferedImage.getHeight()-5-(int)(scale*readings[i]), 10, 10);
-		}
+		printGraph(bufferedImage, readings, middlepoint);
 	}
 
 	@Override
 	public CustomShape mutate(final double[] readings, final double peakvalue){
-		Random RND = new Random();
 		if(RND.nextInt(10) == 0) {
 			return newGenome(readings, peakvalue);
 		}		
@@ -258,11 +236,10 @@ final class CustomShape implements Shape{
 	}
 
 	private double mutateScale(){
-		Random RND = new Random();
 		return (0.99+RND.nextDouble()*0.02);
 	}
 
-	private double getHeightAtPosition(final double position) {
+	double getHeightAtPosition(final double position) {
 		double expectedheight;
 			if(Math.abs(position) < boxlength) {
 				expectedheight = height;
@@ -272,5 +249,52 @@ final class CustomShape implements Shape{
 				expectedheight = height*Math.exp(expcoefficient*(position+boxlength));
 			}
 		return expectedheight;
+	}
+}
+
+final class NormalDistribution extends Shape {
+
+	double scale;
+	double standardDeviation;
+
+	public static NormalDistribution newGenome(final double[] readings, final double peakvalue){
+		return new NormalDistribution(RND.nextDouble()*peakvalue, 
+			RND.nextDouble()*readings.length);
+	}
+
+	private NormalDistribution(final double scale, final double standardDeviation) {
+		this.scale = scale;
+		this.standardDeviation = standardDeviation;
+	}
+
+	double getHeightAtPosition(final double position) {
+		double expectedheight;
+		double heightmultiplier = scale/(standardDeviation*Math.sqrt(2*Math.PI));
+		expectedheight = heightmultiplier*Math.exp(-Math.pow(position, 2)/
+			(2*standardDeviation*standardDeviation));
+		return expectedheight;
+	}
+
+	public double area() {
+		return scale/2;//normal distribution area is 1 but in this case time-scaled
+	}	
+
+	public void printdata(final BufferedImage bufferedImage, final double[] readings, final double middlepoint) {
+		System.out.println("Cost for normal distribution: "+cost(readings, middlepoint));
+		System.out.println("Area for normal distribution: "+area());
+		printGraph(bufferedImage, readings, middlepoint);
+	}
+
+	public NormalDistribution mutate(final double[] readings, final double peakvalue) {
+		if(RND.nextInt(10) == 0) {
+			return newGenome(readings, peakvalue);
+		}		
+		final NormalDistribution child;
+		if(RND.nextBoolean()) {
+			child = new NormalDistribution((0.99+RND.nextDouble()*0.02)*scale, standardDeviation);
+		} else {
+			child = new NormalDistribution(scale, (0.99+RND.nextDouble()*0.02)*standardDeviation);
+		}
+		return child;
 	}
 }
